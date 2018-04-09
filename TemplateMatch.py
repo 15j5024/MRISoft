21503dedd5f6bebad54a4892b6f32e1cbb9d5d78
yayoi K <15j5024@stu.meisei-u.ac.jp>
2018-04-09 10:19:41 +0900

    file organization

--- TemplateMatch.py
+++ TemplateMatch.py
@@ -4,22 +4,46 @@ import numpy as np
 from matplotlib import pyplot as plt
 from nichika import nichika
 
-f = "./hai/nichika/"
-e = "_h_2.jpg"
+def TempMatch(img_src, template):
 
-img_nichi = cv2.imread(f + "hai04" + e)
-#img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-#img_gray = nichika(img_rgb)
-template_nichi = cv2.imread(f + "p2" + e)
-h, w = template_nichi.shape[:2]
+    #画像読み込み
+    #img_dst = cv2.cvtColor(img_src, cv2.COLOR_RGB2GRAY)
+    img_dst = nichika(img_src)
+    w, h = template.shape[::-1]
 
-res = cv2.matchTemplate(img_nichi,template_nichi,cv2.TM_CCOEFF_NORMED)
-threshold = 0.4
-loc = np.where( res >= threshold)
-for pt in zip(*loc[::-1]):
-    cv2.rectangle(img_nichi, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    #テンプレートマッチ
+    res = cv2.matchTemplate(img_dst,template,cv2.TM_CCOEFF_NORMED)
+    threshold = 0.5
+    loc = np.where( res >= threshold)
+    for pt in zip(*loc[::-1]):
+        cv2.rectangle(img_dst, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    
+    return img_dst
 
-cv2.imwrite("dst.jpg",img_nichi)
-cv2.imshow("res", img_nichi)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+
+    hai = ["m1","m2","m3","m4","m5","m6","m7","m8","m9","p1","p2","p3","p4","p5","p6","p7","p8","p9",
+    "s1","s2","s3","s4","s5","s6","s7","s8","s9","jt","jn","js","jp","jhk","jht","jch"]
+
+    bace = "hai04"
+    f1 = "./hai/reduce/"
+    f2 = "./hai/dst/nichi/" + bace + "/"
+    e1 = "_h.jpg"
+    e2 = "_h_dst.jpg"
+    bace = f1 + bace +e1
+
+    for i in hai:
+        name1 = f1 + i + e1
+        name2 = f2 + i + e2
+
+        # 画像の読み込み
+        template = cv2.imread(name1, 0)
+        img_bace = cv2.imread(bace)
+        #マッチング
+        img_dst = TempMatch(img_bace, template)
+        
+        cv2.imwrite(name2,img_dst)
+        #cv2.imshow("res", img_nichi)
+        #cv2.waitKey(0)
+        #cv2.destroyAllWindows()
+
--- nichika.py
+++ nichika.py
@@ -2,18 +2,15 @@
  
 import cv2
 import numpy as np
- 
 
 def nichika(img):
-
-    # グレースケールに変換
+    
+    # グレースケール変換
     img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    #img_gray = img
     # 二値変換
     thresh = 110
     max_pixel = 255
     ret, img_dst = cv2.threshold(img_gray,thresh,max_pixel,cv2.THRESH_BINARY)
-
     return img_dst
 
 if __name__ == '__main__':
@@ -21,7 +18,7 @@ if __name__ == '__main__':
     hai = ["m1","m2","m3","m4","m5","m6","m7","m8","m9","p1","p2","p3","p4","p5","p6","p7","p8","p9",
     "s1","s2","s3","s4","s5","s6","s7","s8","s9","jt","jn","js","jp","jhk","jht","jch"]
 
-    #hai = ["hai01","hai02","hai03","hai04"]
+    #hai.extend(["hai01","hai02","hai03","hai04"])
 
     f1 = "./hai/reduce/"
     f2 = "./hai/nichika/"
